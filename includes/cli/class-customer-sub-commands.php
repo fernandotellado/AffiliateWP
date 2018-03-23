522331ddbdca699b818ab7c9df9053f9d1d64a4a
Drew Jaynes <drew@affiliatewp.com>
2018-03-23 12:04:23 -0600

    Wrap the WP_CLI::add_command() call in a try/catch block.

--- includes/cli/class-customer-sub-commands.php
+++ includes/cli/class-customer-sub-commands.php
@@ -331,4 +331,12 @@ class Sub_Commands extends Base {
 
 }
 
-\WP_CLI::add_command( 'affwp customer', 'AffWP\Customer\CLI\Sub_Commands' );
+try {
+
+	\WP_CLI::add_command( 'affwp customer', 'AffWP\Customer\CLI\Sub_Commands' );
+
+} catch( \Exception $exception ) {
+
+	affiliate_wp()->utils->log( $exception->getCode() . ' - ' . $exception->getMessage() );
+
+}
