21e2121e617fbf6cf97c1ab2bc2bb8e02c51c439
Pippin Williamson <pippin@pippinsplugins.com>
2018-03-28 08:21:10 -0500

    Retain field values when opt-in has an error. #2511

--- templates/opt-in.php
+++ templates/opt-in.php
@@ -1,6 +1,9 @@
 <?php
 global $affwp_opt_in_redirect;
 affiliate_wp()->integrations->opt_in->print_errors();
+$first_name = isset( $_POST['affwp_first_name'] ) ? sanitize_text_field( $_POST['affwp_first_name'] ) : '';
+$last_name  = isset( $_POST['affwp_last_name'] ) ? sanitize_text_field( $_POST['affwp_last_name'] ) : '';
+$email      = isset( $_POST['affwp_email'] ) && is_email( $_POST['affwp_email'] ) ? sanitize_text_field( $_POST['affwp_email'] ) : '';
 
 if ( isset( $_GET['affwp-notice'] ) && 'opted-in' == $_GET['affwp-notice'] ) : ?>
 	<p class="affwp-notice"><?php _e( 'You have subscribed successfully.', 'affiliate-wp' ); ?></p>
@@ -16,17 +19,17 @@ endif ?>
 
 		<p>
 			<label for="affwp-opt-in-name"><?php _e( 'First Name', 'affiliate-wp' ); ?></label>
-			<input id="affwp-opt-in-name" class="required" type="text" name="affwp_first_name" title="<?php esc_attr_e( 'First Name', 'affiliate-wp' ); ?>" />
+			<input id="affwp-opt-in-name" class="required" type="text" name="affwp_first_name" title="<?php esc_attr_e( 'First Name', 'affiliate-wp' ); ?>" value="<?php esc_attr_e( $first_name ); ?>"/>
 		</p>
 
 		<p>
 			<label for="affwp-opt-in-name"><?php _e( 'Last Name', 'affiliate-wp' ); ?></label>
-			<input id="affwp-opt-in-name" class="required" type="text" name="affwp_last_name" title="<?php esc_attr_e( 'Last Name', 'affiliate-wp' ); ?>" />
+			<input id="affwp-opt-in-name" class="required" type="text" name="affwp_last_name" title="<?php esc_attr_e( 'Last Name', 'affiliate-wp' ); ?>" value="<?php esc_attr_e( $last_name ); ?>"/>
 		</p>
 
 		<p>
 			<label for="affwp-opt-in-email"><?php _e( 'Email Address', 'affiliate-wp' ); ?></label>
-			<input id="affwp-opt-in-email" class="required" type="text" name="affwp_email" title="<?php esc_attr_e( 'Email Address', 'affiliate-wp' ); ?>" />
+			<input id="affwp-opt-in-email" class="required" type="text" name="affwp_email" title="<?php esc_attr_e( 'Email Address', 'affiliate-wp' ); ?>" value="<?php esc_attr_e( $email ); ?>"/>
 		</p>
 
 		<p>
